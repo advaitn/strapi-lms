c24d41345a49ccdab44a970af7059834b8df2c26
Advait Nandeshwar <advait.nandeshwar@gmail.com>
2026-01-19 22:26:34 +0530

    feat: Add populate support for modules, lessons, quizzes, questions
    - Override find/findOne in controllers to parse populate parameter
    - Whitelist allowed relations for each content type

--- src/api/module/controllers/module.ts
+++ src/api/module/controllers/module.ts
@@ -1,4 +1,47 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::module.module');
+export default factories.createCoreController('api::module.module', ({ strapi }) => ({
+  async find(ctx) {
+    const allowedPopulates = ['course', 'lessons'];
+    const requestedPopulate = ctx.query.populate;
+    
+    if (requestedPopulate) {
+      if (typeof requestedPopulate === 'string') {
+        const populateFields = requestedPopulate.split(',').map(f => f.trim());
+        const validPopulates: any = {};
+        
+        for (const field of populateFields) {
+          if (allowedPopulates.includes(field)) {
+            validPopulates[field] = true;
+          }
+        }
+        
+        ctx.query.populate = validPopulates;
+      }
+    }
+    
+    return await super.find(ctx);
+  },
 
+  async findOne(ctx) {
+    const allowedPopulates = ['course', 'lessons'];
+    const requestedPopulate = ctx.query.populate;
+    
+    if (requestedPopulate) {
+      if (typeof requestedPopulate === 'string') {
+        const populateFields = requestedPopulate.split(',').map(f => f.trim());
+        const validPopulates: any = {};
+        
+        for (const field of populateFields) {
+          if (allowedPopulates.includes(field)) {
+            validPopulates[field] = true;
+          }
+        }
+        
+        ctx.query.populate = validPopulates;
+      }
+    }
+    
+    return await super.findOne(ctx);
+  },
+}));
--- src/api/question/controllers/question.ts
+++ src/api/question/controllers/question.ts
@@ -1,4 +1,47 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::question.question');
+export default factories.createCoreController('api::question.question', ({ strapi }) => ({
+  async find(ctx) {
+    const allowedPopulates = ['quiz'];
+    const requestedPopulate = ctx.query.populate;
+    
+    if (requestedPopulate) {
+      if (typeof requestedPopulate === 'string') {
+        const populateFields = requestedPopulate.split(',').map(f => f.trim());
+        const validPopulates: any = {};
+        
+        for (const field of populateFields) {
+          if (allowedPopulates.includes(field)) {
+            validPopulates[field] = true;
+          }
+        }
+        
+        ctx.query.populate = validPopulates;
+      }
+    }
+    
+    return await super.find(ctx);
+  },
 
+  async findOne(ctx) {
+    const allowedPopulates = ['quiz'];
+    const requestedPopulate = ctx.query.populate;
+    
+    if (requestedPopulate) {
+      if (typeof requestedPopulate === 'string') {
+        const populateFields = requestedPopulate.split(',').map(f => f.trim());
+        const validPopulates: any = {};
+        
+        for (const field of populateFields) {
+          if (allowedPopulates.includes(field)) {
+            validPopulates[field] = true;
+          }
+        }
+        
+        ctx.query.populate = validPopulates;
+      }
+    }
+    
+    return await super.findOne(ctx);
+  },
+}));
